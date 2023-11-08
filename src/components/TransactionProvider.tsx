c8d144c8c855d9110091b4f0ffb348d10f127aff
joshith3501 <joshith5103@gmail.com>
2023-11-08 22:39:35 +0530

    create useReducer hook to manage transactions

--- src/components/TransactionProvider.tsx
+++ src/components/TransactionProvider.tsx
@@ -1,57 +1,67 @@
-import React, { useState, useContext, useEffect,Dispatch, SetStateAction, ReactNode } from 'react';
+import React, {
+  useState,
+  useContext,
+  useEffect,
+  useReducer,
+  Dispatch,
+  SetStateAction,
+  ReactNode,
+} from "react";
 
-type TransactionUpdateContextProps = {
-  setTransaction: Dispatch<SetStateAction<null | number>>;
+type TransactionsProps = {
+  id: number;
+  amount: number;
+  type: string;
 };
 
+const transactions: TransactionsProps[] = [
+  { id: 1, amount: 500, type: "expense" },
+];
+const TransactionContext = React.createContext(transactions);
 
-const TransactionContext = React.createContext(0);
-const TransactionUpdateContext = React.createContext<TransactionUpdateContextProps | undefined>(undefined);
-
-
-
-const useTransactionUpdate = () => {
-  const updateContext = useContext(TransactionUpdateContext);
-}
-
-export const useTransaction = () => {
-  const balance = useContext(TransactionContext);
-
-  return balance;
-}
+const TransactionReducer = (transaction: any, action: { type: string, payload: {amount?:number, id?:number} }) => {
+  switch (action.type) {
+    case "ADD_INCOME":
+      return [...transaction,{id:transaction.length+1, amount:action.payload.amount,type:"income"}];
+    case "ADD_EXPENSE":
+      return [...transaction,{id:transaction.length+1,amount:action.payload.amount,type:"expense"}];
+    case "REMOVE_TRANSACTION":
+      return [...transaction].filter((transaction)=>{transaction.id !== action.payload.id});
+    default:
+      return transaction;
+  }
+};
 
 const TransactionProvider = ({ children }: any) => {
+  const [transaction, dispatch] = useReducer(TransactionReducer, transactions);
 
-  const [transaction, setTransaction] = useState<null | number>(null);
-  const [income, setIncome] = useState<null | number>(null);
-  const [expense, setExpense] = useState<null | number>(null);
-  const balance: number = (income ?? 0) - (expense ?? 0);
+  const addIncomeTransaction = (e: number) => {
+    dispatch({ type: "ADD_INCOME", payload: { amount: e } });
+  };
 
-  useEffect (()=>{
-    if(transaction !== null) {
-      // Do something with the transaction value here
-      if(transaction < 0) {
-        setExpense(transaction*-1);
-        setTransaction(null);
-      } else {
-        setIncome(transaction);
-        setTransaction(null);
-      }
-    }
-  }, [transaction])
+  const addExpenseTransaction = (e:number) => {
+    dispatch({type:"ADD_EXPENSE", payload: {amount: e}});
+  }
 
+  
+  // useEffect (()=>{
+  //   if(transaction !== null) {
+  //     // Do something with the transaction value here
+  //     if(transaction < 0) {
+  //       setExpense(transaction*-1);
+  //       setTransaction(null);
+  //     } else {
+  //       setIncome(transaction);
+  //       setTransaction(null);
+  //     }
+  //   }
+  // }, [transaction])
 
   return (
-
-  <TransactionContext.Provider value={balance}>
-    <TransactionUpdateContext.Provider value={{setTransaction}}>
+    <TransactionContext.Provider value={transaction}>
       {children}
-    </TransactionUpdateContext.Provider>
-  </TransactionContext.Provider>
-
-
-  ) 
-}
-
+    </TransactionContext.Provider>
+  );
+};
 
 export default TransactionProvider;
